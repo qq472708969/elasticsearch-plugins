9b27f2b67fda3ab717f4510f8bc596d9af85348d
zhuzhiqiang04 <zhuzhiqiang04@meituan.com>
2021-07-22 16:34:06 +0800

    修复routing_slot初始化时机问题；之前由于没有直接添加到static的setting集合中，而是直接操作的keySetting； 之前的旧机制导致集群重启时，先检查static的setting集合中是否包含settingKey；后去初始化routing_slot的setting配置。发现静态集合中无此属性，则恢复索引时index.routing_slot前添加了前缀archived； 属性变为了：archived.index.routing_slot进而导致查询时routing失效；从而无法获取该参数进行数据分配。

--- plugin/src/main/java/com/cgroup/esbulkrouting/FastBulkPlugin.java
+++ plugin/src/main/java/com/cgroup/esbulkrouting/FastBulkPlugin.java
@@ -11,9 +11,12 @@ import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.rest.RestController;
 import org.elasticsearch.rest.RestHandler;
 
-import java.util.Arrays;
-import java.util.List;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.*;
 import java.util.function.Supplier;
+
+import static com.cgroup.esbulkrouting.FastBulkAction.SETTING_ROUTING_SLOT;
 
 /**
  * Created by zzq on 2021/7/5.
@@ -31,9 +34,32 @@ public class FastBulkPlugin extends Plugin implements ActionPlugin {
         return Arrays.asList(new ActionHandler<>(FastBulkAction.INSTANCE, FastBulkTransportAction.class));
     }
 
-    public FastBulkPlugin() {
+    public FastBulkPlugin() throws NoSuchFieldException, IllegalAccessException {
         super();
         logger.info("FastBulkPlugin初始化");
+        addRoutingSlotProperty();
+    }
+
+    /**
+     * 最好的时机，向索引setting列表添加routing_slot参数
+     *
+     * @throws NoSuchFieldException
+     * @throws IllegalAccessException
+     */
+    private void addRoutingSlotProperty() throws NoSuchFieldException, IllegalAccessException {
+        Field built_in_index_settingsField = IndexScopedSettings.class.getDeclaredField("BUILT_IN_INDEX_SETTINGS");
+        Field modifiersField = Field.class.getDeclaredField("modifiers");
+        modifiersField.setAccessible(true);
+        modifiersField.setInt(built_in_index_settingsField, built_in_index_settingsField.getModifiers() & ~Modifier.FINAL);
+
+        Setting<Integer> integerSetting = Setting.intSetting(SETTING_ROUTING_SLOT, 1, 1, 1024,
+                Setting.Property.Dynamic, Setting.Property.IndexScope);
+
+        Set<Setting<?>> settingSet = new HashSet<>();
+        settingSet.addAll(IndexScopedSettings.BUILT_IN_INDEX_SETTINGS);
+        settingSet.add(integerSetting);
+
+        built_in_index_settingsField.set(null, settingSet);
     }
 
     @Override
@@ -41,6 +67,6 @@ public class FastBulkPlugin extends Plugin implements ActionPlugin {
                                              ClusterSettings clusterSettings, IndexScopedSettings indexScopedSettings,
                                              SettingsFilter settingsFilter, IndexNameExpressionResolver indexNameExpressionResolver,
                                              Supplier<DiscoveryNodes> nodesInCluster) {
-        return Arrays.asList(new FastBulkRestHandler(settings, restController,indexScopedSettings));
+        return Arrays.asList(new FastBulkRestHandler(settings, restController, indexScopedSettings));
     }
 }
