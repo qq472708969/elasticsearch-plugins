24b5e9526b217606a8078de4d1cb658845f7b45c
zhuzhiqiang04 <zhuzhiqiang04@meituan.com>
2022-03-11 19:43:58 +0800

    使用es restClient  灵活操作所有esAPI

--- plugin/src/main/java/com/cgroup/synclinkrequest/LinkExecutor.java
+++ plugin/src/main/java/com/cgroup/synclinkrequest/LinkExecutor.java
@@ -1,5 +1,9 @@
 package com.cgroup.synclinkrequest;
 
+import org.elasticsearch.client.RestClient;
+
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,13 +12,23 @@ import java.util.List;
 public class LinkExecutor {
 
     private final List<LinkRequest> requests;
+    private final RestClient restClient;
 
-    public LinkExecutor(List<LinkRequest> requests) {
+    public LinkExecutor(List<LinkRequest> requests, RestClient restClient) {
         this.requests = requests;
+        this.restClient = restClient;
     }
 
-    public List<LinkResponse> exec() {
-
-        return null;
+    public List<LinkResponse> exec() throws IOException {
+        List<LinkResponse> resList = new ArrayList<>();
+        for (int i = 0; i < requests.size(); i++) {
+            LinkRequest linkRequest = requests.get(i);
+            LinkResponse linkResponse = linkRequest.execRequest(restClient);
+            //如果没有后续的请求，则直接结束掉
+            if (linkResponse.getState().equals(ResLinkState.None)) {
+                break;
+            }
+        }
+        return resList;
     }
 }
