327e741a6e67defc3c49f74ecf660354242bc9fd
zhuzhiqiang04 <zhuzhiqiang04@meituan.com>
2022-03-23 15:54:36 +0800

    CEP  案例

--- plugin/src/main/java/com/cgroup/Test1.java
+++ plugin/src/main/java/com/cgroup/Test1.java
@@ -1,64 +1,12 @@
 package com.cgroup;
 
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Created by zzq
  */
 public class Test1 {
-    public static class Node {
-        int val;
-        Node next = null;
-
-        public Node(int val) {
-            this.val = val;
-        }
-    }
-
-    /**
-     * p指向原始链表头。list为复制链表
-     * <p>
-     * 首次复制节点，头结点的next为null，并让指针n，指向头结点
-     * <p>
-     * else、
-     * 、
-     *
-     * @param args
-     */
-    public static void main(String[] args) {
-        Node n1 = new Node(1);
-        Node n2 = new Node(2);
-        Node n3 = new Node(3);
-        Node n4 = new Node(4);
-
-        n1.next = n2;
-        n2.next = n3;
-        n3.next = n4;
-        //n4.next=n2;
-
-        Node p = n1;
-        //复制的链表
-        Node ret = null;
-
-        Node n = ret;
-        //向后走
-        for (; p != null; ) {
-            if (ret == null) {
-                ret = new Node(p.val);
-                n = ret;
-            } else {
-                Node tmp = new Node(p.val);
-                n.next = tmp;
-                n = n.next;
-            }
-            p = p.next;
-        }
-
-        Node head = ret;
-
-        for (; head != null; ) {
-            System.out.println(head.next + " - ");
-        }
-
-    }
-
 
 }
---
+++ plugin/src/main/java/com/cgroup/stream/TestCEP.java
@@ -0,0 +1,143 @@
+package com.cgroup.stream;
+
+import org.apache.flink.api.common.restartstrategy.RestartStrategies;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.cep.CEP;
+import org.apache.flink.cep.PatternSelectFunction;
+import org.apache.flink.cep.PatternStream;
+import org.apache.flink.cep.pattern.Pattern;
+import org.apache.flink.cep.pattern.conditions.IterativeCondition;
+import org.apache.flink.cep.pattern.conditions.SimpleCondition;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.runtime.state.filesystem.FsStateBackend;
+import org.apache.flink.shaded.jackson2.org.yaml.snakeyaml.events.Event;
+import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.CheckpointConfig;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
+import org.apache.flink.streaming.api.windowing.time.Time;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by zzq on 2022/3/23.
+ */
+public class TestCEP {
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        conf.setString("taskmanager.numberOfTaskSlots", "24");
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(conf);
+        env.setParallelism(1);
+        //env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+        //开启checkpoint机制，1000毫秒为发送barrier的间隔时长，
+        env.enableCheckpointing(1000L, CheckpointingMode.EXACTLY_ONCE);
+        //保证两次checkpoint操作的最小间隔为500毫秒
+        env.getCheckpointConfig().setMinPauseBetweenCheckpoints(500L);
+        env.getCheckpointConfig().enableExternalizedCheckpoints(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
+        //超时时间5秒
+        env.getCheckpointConfig().setCheckpointTimeout(5000L);
+        // 根据用户指定的数量可以同时触发多个Checkpoint，进而提升Checkpoint整体的效率
+        env.getCheckpointConfig().setMaxConcurrentCheckpoints(1);
+        // 如果有更近的保存点时，是否将作业回退到该检查点
+        env.getCheckpointConfig().setPreferCheckpointForRecovery(true);
+        //env.setStateBackend(new FsStateBackend("file:///D:/elasticsearch-plugins/plugin/src/main"));
+        //恢复（重试2次， 重启之间的延时时间3）
+        env.setRestartStrategy(RestartStrategies.fixedDelayRestart(2, org.apache.flink.api.common.time.Time.of(5, TimeUnit.SECONDS)));
+        /**
+         * 每100毫秒标记一次Watermark
+         */
+        env.getConfig().setAutoWatermarkInterval(100L);
+
+
+        DataStreamSource<String> stringDataStreamSource = env.addSource(new SourceFunction<String>() {
+
+            String ph = "13687980889";
+            String state0 = "success";
+            String state1 = "fail";
+            boolean ret = true;
+            int count = 0;
+            int error = 0;
+
+            @Override
+            public void run(SourceContext<String> sourceContext) throws Exception {
+
+                for (; ret; ) {
+                    if (count > 10) {
+                        sourceContext.collect(ph + "," + state1);
+                        error++;
+                        if (error > 4) {
+                            count = 0;
+                            error = 0;
+                        }
+                        Thread.sleep(599L);
+                        continue;
+                    }
+
+                    sourceContext.collect(ph + "," + state0);
+                    count++;
+
+
+                    Thread.sleep(599L);
+                }
+            }
+
+            @Override
+            public void cancel() {
+                ret = false;
+            }
+        });
+
+        Pattern<String, String> patternLogin = Pattern.
+
+                <String>begin("第一次").where(new SimpleCondition<String>() {
+            @Override
+            public boolean filter(String value) throws Exception {
+                return value.split("\\,")[1].contains("fail");
+            }
+        })
+                .next("第二次").where(new SimpleCondition<String>() {
+                    @Override
+                    public boolean filter(String value) throws Exception {
+                        return value.split("\\,")[1].contains("fail");
+                    }
+                })
+
+                .next("第三次").where(new SimpleCondition<String>() {
+                    @Override
+                    public boolean filter(String value) throws Exception {
+                        return value.split("\\,")[1].contains("fail");
+                    }
+                }).within(Time.seconds(2));
+
+        KeyedStream<String, String> stringStringKeyedStream = stringDataStreamSource
+                .keyBy(new KeySelector<String, String>() {
+                    @Override
+                    public String getKey(String value) throws Exception {
+                        return value.split("\\,")[0];
+                    }
+                });
+
+        PatternStream<String> patternStream = CEP.pattern(stringStringKeyedStream
+                , patternLogin);
+
+        SingleOutputStreamOperator<String> select = patternStream.select(new PatternSelectFunction<String, String>() {
+            @Override
+            public String select(Map<String, List<String>> pattern) throws Exception {
+                return "1：" + pattern.get("第一次").get(0) +
+                        " 2：" + pattern.get("第二次").get(0) +
+                        " 3：" + pattern.get("第三次").get(0)
+                        ;
+            }
+        });
+        //stringStringKeyedStream.print();
+        select.print();
+
+        env.execute();
+    }
+}
